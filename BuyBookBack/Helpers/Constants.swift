748c945dce1d328d38c0b60d74ea9624d4e1e9a8
Bilal Fakhro <51514054+Bilalfakhro@users.noreply.github.com>
2020-09-20 00:21:38 +0200

    Added SignUp, Login with Firebase and navigation to HomeScreen. Media file to SplashScreen

---
+++ BuyBookBack/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  BuyBookBack
+//
+//  Created by Bilal Fakhro on 2020-09-19.
+//  Copyright © 2020 Bilal Fakhro. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+        
+        static let homeViewController = "HomeVC"
+        
+    }
+    
+    
+}
